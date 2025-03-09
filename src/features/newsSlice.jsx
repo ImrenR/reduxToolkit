c17752eac84f2a39c078ae5ed8196ea4e4867644
ImrenR <imrenrahba@gmail.com>
2025-03-09 11:25:22 +0100

    news

---
+++ src/features/newsSlice.jsx
@@ -0,0 +1,29 @@
+import { createAsyncThunk,createSlice} from "@reduxjs/toolkit";
+import axios from "axios";
+
+
+
+export const getirData=createAsyncThunk(
+  "newsSlice/getirData", async ()=> {
+ const res= await axios(  "https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=81a4163ea7eb4bccb489151972100adb" 
+);
+return res
+}
+);
+
+export const newsSlice=createSlice({
+  name: "newsSlice",
+  initialState: {
+  news:[],
+  loading:true
+},
+reducers:{},
+
+extraReducers: (builder)=> {
+  builder
+  .addCase()
+  .addCase()
+}
+})
+
+export default newsSlice.reducer
